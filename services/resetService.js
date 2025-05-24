b0cad7610fefb2914b86b813e5798f07988d9d67
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-05-25 02:41:34 +0300

    Update resetService.js

--- services/resetService.js
+++ services/resetService.js
@@ -1,21 +1,21 @@
 const Employee = require('../models/Employee');
 
-const resetEmployeeBalances = async () => {
-  try {
-    await Employee.updateMany(
-      {},
-      {
-        $set: {
-          salary: 0,
-          advanceBalance: 0,
-        },
-      },
-    );
+// const resetEmployeeBalances = async () => {
+//   try {
+//     await Employee.updateMany(
+//       {},
+//       {
+//         $set: {
+//           salary: 0,
+//           advanceBalance: 0,
+//         },
+//       },
+//     );
 
-    console.log('🔔 Tüm çalışanların maaş ve avans bakiyeleri sıfırlandı.');
-  } catch (error) {
-    console.error('Resetleme sırasında hata oluştu:', error.message);
-  }
-};
+//     console.log('🔔 Tüm çalışanların maaş ve avans bakiyeleri sıfırlandı.');
+//   } catch (error) {
+//     console.error('Resetleme sırasında hata oluştu:', error.message);
+//   }
+// };
 
-module.exports = { resetEmployeeBalances };
+// module.exports = { resetEmployeeBalances };
